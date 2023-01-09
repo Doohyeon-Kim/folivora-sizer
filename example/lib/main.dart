aacbccf2ae255b59acaedc9006654bf489286679
Doohyeon Kim <me@doohyeon.kim>
2023-01-09 18:56:38 +0900

    Implemented Responsive Sizer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:folivora_sizer/folivora_sizer.dart';
+import 'root_page.dart';
 import 'theme_view_model.dart';
 
 void main() {
@@ -18,83 +18,10 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Folivora Package Test App',
           theme: themeData,
-          home: const MyHomePage(title: 'Folivora'),
+          home: const RootPage(),
         );
       },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    Sizer.init(standardLogicalWidth: 393, standardLogicalHeight: 932);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("window width: ${Sizer.unitWidth.window}"),
-                const SizedBox(
-                  width: 24,
-                ),
-                Text("window height: ${Sizer.unitHeight.window}"),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("standard width: ${Sizer.unitWidth.standard}"),
-                const SizedBox(
-                  width: 24,
-                ),
-                Text("standard height: ${Sizer.unitHeight.standard}"),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("logical width 24: 24"),
-                SizedBox(
-                  width: 24,
-                ),
-                Text("logical height 24: 24"),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Text("unit width 24: ${Sizer.unitWidth.lp24}"),
-            const SizedBox(
-              height: 8,
-            ),
-            Text("unit height 24: ${Sizer.unitHeight.lp24}"),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- lib/sizer/sizer.dart
+++ lib/sizer/sizer.dart
@@ -8,10 +8,13 @@ class Sizer {
   static UnitWidth unitWidth = UnitWidth();
   static UnitHeight unitHeight = UnitHeight();
   static UnitPadding unitPadding = UnitPadding();
-
-  static Responsive responsive = Responsive();
+  static double? _standardLogicalWidth;
+  static double? _standardLogicalHeight;
 
   static init({double? standardLogicalWidth, double? standardLogicalHeight}) {
+    _standardLogicalWidth = standardLogicalWidth;
+    _standardLogicalHeight = standardLogicalHeight;
+
     unitPadding.topSafeArea =
         MediaQueryData.fromWindow(WidgetsBinding.instance.window).padding.top;
     unitPadding.bottomSafeArea =
@@ -21,17 +24,14 @@ class Sizer {
     unitPadding.safeAreaPadding =
         unitPadding.topSafeArea + unitPadding.bottomSafeArea;
 
-    unitWidth.window =
+    unitWidth.max =
         MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.width;
 
-    unitHeight.window =
+    unitHeight.max =
         MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height;
 
-    unitWidth.standard = standardLogicalWidth ?? unitWidth.window;
-    unitHeight.standard = standardLogicalHeight ?? unitHeight.window;
-
-    unitWidth.max = unitWidth.window;
-    unitHeight.max = unitHeight.window;
+    unitWidth.standard = _standardLogicalWidth ?? unitWidth.max;
+    unitHeight.standard = _standardLogicalHeight ?? unitHeight.max;
 
     unitWidth.lp4 = unitWidth.max / (unitWidth.standard / 4);
     unitHeight.lp4 = unitHeight.max / (unitHeight.standard / 4);
@@ -94,4 +94,92 @@ class Sizer {
   }
 }
 
-class Responsive {}
+class ResponsiveSizer {
+  UnitWidth unitWidth = UnitWidth();
+  UnitHeight unitHeight = UnitHeight();
+  UnitPadding unitPadding = UnitPadding();
+  late BuildContext context;
+
+  ResponsiveSizer({required this.context}) {
+    measure();
+  }
+
+  measure() {
+    unitPadding.topSafeArea =
+        MediaQueryData.fromWindow(WidgetsBinding.instance.window).padding.top;
+    unitPadding.bottomSafeArea =
+        MediaQueryData.fromWindow(WidgetsBinding.instance.window)
+            .padding
+            .bottom;
+    unitPadding.safeAreaPadding =
+        unitPadding.topSafeArea + unitPadding.bottomSafeArea;
+
+    unitWidth.window = MediaQuery.of(context).size.width;
+
+    unitHeight.window = MediaQuery.of(context).size.height;
+
+    unitWidth.max = unitWidth.window;
+    unitHeight.max = unitHeight.window;
+
+    unitWidth.lp4 =
+        unitWidth.max / ((Sizer._standardLogicalWidth ?? unitWidth.max) / 4);
+    unitHeight.lp4 =
+        unitHeight.max / ((Sizer._standardLogicalHeight ?? unitHeight.max) / 4);
+
+    unitWidth.lp8 = unitWidth.lp4 * 2;
+    unitWidth.lp12 = unitWidth.lp4 * 3;
+    unitWidth.lp16 = unitWidth.lp4 * 4;
+    unitWidth.lp20 = unitWidth.lp4 * 5;
+    unitWidth.lp24 = unitWidth.lp4 * 6;
+    unitWidth.lp28 = unitWidth.lp4 * 7;
+    unitWidth.lp32 = unitWidth.lp4 * 8;
+    unitWidth.lp36 = unitWidth.lp4 * 9;
+    unitWidth.lp40 = unitWidth.lp4 * 10;
+    unitWidth.lp44 = unitWidth.lp4 * 11;
+    unitWidth.lp48 = unitWidth.lp4 * 12;
+    unitWidth.lp52 = unitWidth.lp4 * 13;
+    unitWidth.lp56 = unitWidth.lp4 * 14;
+    unitWidth.lp60 = unitWidth.lp4 * 15;
+    unitWidth.lp64 = unitWidth.lp4 * 16;
+    unitWidth.lp68 = unitWidth.lp4 * 17;
+    unitWidth.lp72 = unitWidth.lp4 * 18;
+    unitWidth.lp76 = unitWidth.lp4 * 19;
+    unitWidth.lp80 = unitWidth.lp4 * 20;
+    unitWidth.lp84 = unitWidth.lp4 * 21;
+    unitWidth.lp88 = unitWidth.lp4 * 22;
+    unitWidth.lp92 = unitWidth.lp4 * 23;
+    unitWidth.lp96 = unitWidth.lp4 * 24;
+    unitWidth.lp100 = unitWidth.lp4 * 25;
+    unitWidth.lp104 = unitWidth.lp4 * 26;
+    unitWidth.lp108 = unitWidth.lp4 * 27;
+    unitWidth.lp112 = unitWidth.lp4 * 28;
+
+    unitHeight.lp8 = unitHeight.lp4 * 2;
+    unitHeight.lp12 = unitHeight.lp4 * 3;
+    unitHeight.lp16 = unitHeight.lp4 * 4;
+    unitHeight.lp20 = unitHeight.lp4 * 5;
+    unitHeight.lp24 = unitHeight.lp4 * 6;
+    unitHeight.lp28 = unitHeight.lp4 * 7;
+    unitHeight.lp32 = unitHeight.lp4 * 8;
+    unitHeight.lp36 = unitHeight.lp4 * 9;
+    unitHeight.lp40 = unitHeight.lp4 * 10;
+    unitHeight.lp44 = unitHeight.lp4 * 11;
+    unitHeight.lp48 = unitHeight.lp4 * 12;
+    unitHeight.lp52 = unitHeight.lp4 * 13;
+    unitHeight.lp56 = unitHeight.lp4 * 14;
+    unitHeight.lp60 = unitHeight.lp4 * 15;
+    unitHeight.lp64 = unitHeight.lp4 * 16;
+    unitHeight.lp68 = unitHeight.lp4 * 17;
+    unitHeight.lp72 = unitHeight.lp4 * 18;
+    unitHeight.lp76 = unitHeight.lp4 * 19;
+    unitHeight.lp80 = unitHeight.lp4 * 20;
+    unitHeight.lp84 = unitHeight.lp4 * 21;
+    unitHeight.lp88 = unitHeight.lp4 * 22;
+    unitHeight.lp92 = unitHeight.lp4 * 23;
+    unitHeight.lp96 = unitHeight.lp4 * 24;
+    unitHeight.lp100 = unitHeight.lp4 * 25;
+    unitHeight.lp104 = unitHeight.lp4 * 26;
+    unitHeight.lp108 = unitHeight.lp4 * 27;
+    unitHeight.lp112 = unitHeight.lp4 * 28;
+  }
+}
